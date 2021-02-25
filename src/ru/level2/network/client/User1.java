4fb9dde1cebe279b6896c9da552ada5355158f91
mvit <mvit_svk@mail.ru>
2021-02-25 20:01:26 +0300

    network chat version2

---
+++ src/ru/level2/network/client/User1.java
@@ -0,0 +1,8 @@
+package ru.level2.network.client;
+
+public class User1 {
+    public static void main(String[] args) {
+        new Client("localhost",8080);
+    }
+
+}
---
+++ src/ru/level2/network/client/User3.java
@@ -0,0 +1,7 @@
+package ru.level2.network.client;
+
+public class User3 {
+    public static void main(String[] args) {
+        new Client("localhost",8080);
+    }
+}
